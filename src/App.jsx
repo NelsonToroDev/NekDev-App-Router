3ef0801d56f08d5548790607a76f44b295a10c28
Nelson Toro <nelson.toro.flores@gmail.com>
2024-02-11 19:43:07 -0400

    Cleanning up the code creating pages on its own files

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,8 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import { EVENTS } from './consts.js'
-
-function navigate(href) {
-  window.history.pushState({}, '', href)
-
-  // create a custom event to notify that URL was changed
-  const navigationEvent = new Event(EVENTS.PUSHEVENT)
-  window.dispatchEvent(navigationEvent) // Trigger our custom event
-}
-
-function HomePage() {
-  return (
-    <>
-      <h1>Home</h1>
-      <p>This is a sample page to create a React Router from zero</p>
-      <button onClick={() => navigate('/about')}>About us</button>
-    </>
-  )
-}
-
-function AboutPage() {
-  return (
-    <>
-      <h1>About</h1>
-      <div>
-        <img
-          src='https://avatars.githubusercontent.com/u/4542698?s=400&u=a0e773d539532a734c0840c94442d20faf99f833&v=4'
-          alt='About pic'
-        />
-        <p>Hello this a React Router clone</p>
-      </div>
-      <button onClick={() => navigate('/')}>Go to home</button>
-    </>
-  )
-}
+import HomePage from './pages/Home.jsx'
+import AboutPage from './pages/About.jsx'
 
 function App() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+import { navigate } from '../navigate'
+
+export default function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <div>
+        <img
+          src='https://avatars.githubusercontent.com/u/4542698?s=400&u=a0e773d539532a734c0840c94442d20faf99f833&v=4'
+          alt='About pic'
+        />
+        <p>Hello this a React Router clone</p>
+      </div>
+      <button onClick={() => navigate('/')}>Go to home</button>
+    </>
+  )
+}
