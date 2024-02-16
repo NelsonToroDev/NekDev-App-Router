bc70eba9787946e69e888cb943190236613f4af3
Nelson Toro <nelson.toro.flores@gmail.com>
2024-02-16 14:51:37 -0400

    Added useQueryParams hooks

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,16 @@
 export { Router } from './components/Router'
 export { Route } from './components/Route'
 export { Link } from './components/Link'
+
+// Isomorphic to be use on server side rendering and client side rendering
+export function useQueryParams({ req } = {}) {
+  const isServer = typeof window === 'undefined'
+  if (isServer) {
+    const { query } = req
+    return query
+  }
+
+  const search = window.location.search
+  const params = new URLSearchParams(search)
+  return Object.fromEntries(params.entries())
+}
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,10 @@
 import { useEffect } from 'react'
 import { Link } from '../components/Link'
+import { useQueryParams } from '..'
 
 export default function SearchPage({ routeParams }) {
+  const queryParams = useQueryParams()
+
   useEffect(() => {
     document.title = `looking for ${routeParams.query}`
   }, [])
@@ -9,7 +12,17 @@ export default function SearchPage({ routeParams }) {
   return (
     <>
       <h3>New page for query using parameters</h3>
-      <h4>Query Param: {routeParams.query}</h4>
+      <h4>Route Param: {routeParams.query}</h4>
+      <div>
+        <h4>Query Params:</h4>
+        <ul>
+          {Object.entries(queryParams).map(([paramName, paramValue], index) => (
+            <li key={index}>
+              {paramName}: {paramValue}
+            </li>
+          ))}
+        </ul>
+      </div>
       <Link to='/'>Back to Home</Link>
     </>
   )
